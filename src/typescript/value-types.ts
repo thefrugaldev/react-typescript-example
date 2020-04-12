157aabc48094c9f192db3bfbfbd2f39d8492f5f8
Clayton Orman <claytonkfc@Claytons-MacBook-Pro.local>
2020-04-12 16:53:49 -0400

    :electric_plug: differentiating between types with type guards

---
+++ src/typescript/value-types.ts
@@ -0,0 +1,43 @@
+import { Project, TextLayer, ImageLayer, LayerType, Size } from "./types";
+
+const projectSize: Size = {
+  width: 512,
+  height: 250,
+};
+
+const textLayer: TextLayer = {
+  type: LayerType.Text,
+  maxWidth: 1000,
+  position: { x: 128, y: 208 },
+  color: "#e8166d",
+  id: "10",
+  rotation: 0,
+  text: "Advanced TypeScript",
+  fontSize: "20px",
+};
+
+const imageLayer: ImageLayer = {
+  type: LayerType.Image,
+
+  position: { x: 0, y: 0 },
+  id: "20",
+  rotation: 0,
+  src: "ps-dark.png",
+  maxBounds: { width: projectSize.width },
+};
+
+function setFontSize(layer: TextLayer, value: string | number) {
+  if (typeof value === "number") {
+    // Note: checks type of primitive objects
+    layer.fontSize = `${value}px`;
+  } else {
+    layer.fontSize = value;
+  }
+}
+
+const project: Project = {
+  layers: [imageLayer, textLayer],
+  size: projectSize,
+};
+
+setFontSize(textLayer, "20em");
